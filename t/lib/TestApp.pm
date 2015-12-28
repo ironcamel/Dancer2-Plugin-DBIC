3fa9925905a0065686c50d927892a285923d7a25
Peter Mottram (SysPete) <peter@sysnix.com>
2015-12-28 18:11:16 +0100

    Remove subroutine import list from test 04
    The new (soon to be released) Dancer2::Plugin code no longer allows
    subroutine import list to be supplied when plugin is used.

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -1,7 +1,7 @@
 package t::lib::TestApp;
 
 use Dancer2;
-use Dancer2::Plugin::DBIC qw(schema resultset);
+use Dancer2::Plugin::DBIC;
 
 get '/' => sub {
     my $total_user = schema->resultset('User')->search();
